7f774fcfe2e90cfb18f22e20e3a08e7bd6f720b8
alla2001 <48186577+alla2001@users.noreply.github.com>
2022-07-01 17:46:30 +0200

    Update ZoneLimite.cs

--- Assets/ZoneLimite.cs
+++ Assets/ZoneLimite.cs
@@ -8,6 +8,7 @@ public class ZoneLimite : MonoBehaviour
     public static ZoneLimite instance;
     public Image Player;
     public RawImage Map;
+    private Vector3 normlizedPosOnMap;
 
     private void Awake()
     {
@@ -19,16 +20,15 @@ public class ZoneLimite : MonoBehaviour
 
     public bool OnMap(Vector2 screenPos)
     {
-        Vector3 pos = new Vector3(screenPos.x, screenPos.y, 0) -
-            (new Vector3(
-                Map.GetComponent<BoxCollider2D>().offset.x,
-                Map.GetComponent<BoxCollider2D>().offset.y, 0) +
-                Map.transform.position + new Vector3(Screen.width / 2, Screen.height / 4));
-        print(screenPos);
-        if (pos.x >= -Map.GetComponent<BoxCollider2D>().size.x / 2 &&
-            pos.x <= Map.GetComponent<BoxCollider2D>().size.x / 2 &&
-            pos.y >= -Map.GetComponent<BoxCollider2D>().size.y / 2
-            && pos.y <= Map.GetComponent<BoxCollider2D>().size.y / 2)
+        Vector2 mapPos = Map.rectTransform.position + new Vector3(Screen.width / 2, Screen.height / 2, 0);
+        Vector3 posOnMap = screenPos - mapPos;
+
+        normlizedPosOnMap = new Vector2(posOnMap.x / Map.rectTransform.rect.width, posOnMap.y / Map.rectTransform.rect.height);
+
+        if (normlizedPosOnMap.x >= 0 &&
+            normlizedPosOnMap.x <= 1 &&
+           normlizedPosOnMap.y >= 0 &&
+           normlizedPosOnMap.y <= 1)
         {
             return true;
         }
@@ -42,30 +42,17 @@ public class ZoneLimite : MonoBehaviour
 
     public Vector3 GetPointInZone(Vector2 screenPos)
     {
+        Vector2 temp = new Vector2(Mathf.Clamp(normlizedPosOnMap.x, 0, 1), Mathf.Clamp(normlizedPosOnMap.y, 0, 1));
         Player.rectTransform.localPosition = new Vector3(
-            Map.GetComponent<BoxCollider2D>().size.x * (screenPos.x - 0.5f),
-            Map.GetComponent<BoxCollider2D>().size.y * (screenPos.y - 0.5f), 0)
-            +
-            new Vector3(
-                Map.GetComponent<BoxCollider2D>().offset.x,
-                Map.GetComponent<BoxCollider2D>().offset.y, 0);
-
-        Vector3 post = new Vector3(
-            Map.GetComponent<BoxCollider2D>().size.x * (screenPos.x - 0.5f),
-            Map.GetComponent<BoxCollider2D>().size.y * (screenPos.y - 0.5f), 0)
-            +
-            new Vector3(
-                Map.GetComponent<BoxCollider2D>().offset.x,
-                Map.GetComponent<BoxCollider2D>().offset.y, 0);
-
-        screenPos = new Vector2(post.x / Map.GetComponent<BoxCollider2D>().size.x, post.y / Map.GetComponent<BoxCollider2D>().size.y);
+            temp.x * Map.rectTransform.rect.width,
+            temp.y * Map.rectTransform.rect.height, 0);
 
         Vector3 center = transform.TransformPoint(GetComponent<BoxCollider>().center);
         BoxCollider collider = GetComponent<BoxCollider>();
         Vector3 pos = new Vector3(
-            center.x - collider.bounds.size.x / 2 + collider.bounds.size.x * screenPos.x,
+            center.x - collider.bounds.size.x / 2 + collider.bounds.size.x * temp.x,
            MovementController.instance.transform.position.y,
-          center.z - collider.bounds.size.z / 2 + collider.bounds.size.z * screenPos.y);
+          center.z - collider.bounds.size.z / 2 + collider.bounds.size.z * temp.y);
 
         return pos;
     }
